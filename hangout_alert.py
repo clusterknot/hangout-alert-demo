ddfe19074888748bd2bf0dcee686afa774b3f707
Alok Anand <alok@almabetter.com>
2021-04-06 15:17:18 +0530

    Update hangout_alert.py

--- hangout_alert.py
+++ hangout_alert.py
@@ -2,7 +2,7 @@ from httplib2 import Http
 from json import dumps
 
 def alert(filename):
-    url = "https://chat.googleapis.com/v1/spaces/AAAAMPGdQ5M/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=KFGqEGq3dpyLd9kT1MCRHe5i-FwQgkzSNizC6uS8NAA%3D"
+    url = ""
 
     lines = ''
     with open(filename) as file_in:
